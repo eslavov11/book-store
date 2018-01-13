463ed7e50bde510669261400566fc3fd84e93157
Edward <e.slavov11@gmail.com>
2018-01-14 00:34:01 +0200

    navbar component added

--- src/admin-portal/src/app/app.module.ts
+++ src/admin-portal/src/app/app.module.ts
@@ -2,16 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule} from '@angular/http';
-import { MaterialModule } from '@angular/material';
+import { MaterialModule } from './material.module';
 import 'hammerjs';
 
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
